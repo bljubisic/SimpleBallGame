481ea1d5b2cb714afc9ff513db950d459234d353
Bratislav Ljubisic <bratislav.ljubisic@icloud.com>
2025-06-26 19:04:26 +0200

    Initial Commit

---
+++ SimpleBallGame/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  SimpleBallGame
+//
+//  Created by Bratislav Ljubisic Home  on 6/26/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ SimpleBallGame/SimpleBallGameApp.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleBallGameApp.swift
+//  SimpleBallGame
+//
+//  Created by Bratislav Ljubisic Home  on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBallGameApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
